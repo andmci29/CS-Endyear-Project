3fdeb0f7d5f134c84edc0bc15ca15a7881ef7f69
ejlaforest10 <ejlaforest10@gmail.com>
2025-06-03 13:15:13 -0400

    Added Blender models

---
+++ CS Endyear/Assets/Scripts/sphereFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class sphereFollow : MonoBehaviour
+{
+    public GameObject sphere;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = sphere.transform.position; 
+    }
+}
