c1bd36974c37a76068199891af015470801200c2
Andrew McIlroy <amcilroy2009@gmail.com>
2025-06-10 13:22:25 -0400

    Various updates

--- CS Endyear/Assets/Scripts/Menu/PlayButton.cs
+++ CS Endyear/Assets/Scripts/Menu/PlayButton.cs
@@ -6,4 +6,26 @@ public class PlayButton : MonoBehaviour
     {
         SceneManager.LoadScene("3D");
     }
+
+    public void Arcade3D()
+    {
+        SceneManager.LoadScene("3D Arcade");
+    }
+
+    public void Arcade2D()
+    {
+        SceneManager.LoadScene("2D Arcade");
+    }
+
+    public void ArcadeMenu()
+    {
+        SceneManager.LoadScene("ArcadeMenu");
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
 }
